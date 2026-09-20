aade662d818d7715af5ac2b399c2f1238837dc03
Wojciech Wojcik <wojcik.wojtek@yahoo.pl>
2026-09-20 20:05:14 +0200

    feat: add admin file upload

--- src/Controller/Admin/GalleryController.php
+++ src/Controller/Admin/GalleryController.php
@@ -5,6 +5,11 @@ namespace App\Controller\Admin;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use App\Service\FileUploader;
+
 
 final class GalleryController extends AbstractController
 {
@@ -29,6 +34,32 @@ final class GalleryController extends AbstractController
     {
         return $this->render('admin/gallery/index.html.twig', [
             'title' => 'Decorations',
+        ]);
+    }
+
+    #[Route('/admin/upload', name: 'admin_upload')]
+    public function upload(Request $request, FileUploader $fileUploader): JsonResponse
+    {
+        // Get list of uploaded files
+        $files = $request->files->all('images');
+
+        $uploadedFiles = [];
+
+        // Loop over fiels and save on server
+        foreach ($files as $file) {
+            // need to create unique name standard plus timestamp
+            if (!$file instanceof UploadedFile) {
+                continue;
+            }
+
+            $filename = $fileUploader->upload($file);
+
+            $uploadedFiles[] = $filename;
+        }
+        return new JsonResponse([
+            'success' => true,
+            'message' => 'test',
+            'files' => $uploadedFiles,
         ]);
     }
 }
---
+++ src/Enum/GalleryType.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enum;
+
+enum GalleryType: int
+{
+    case BACKDROPS = 1;
+    case ANIMALS = 2;
+    case DECORATIONS = 3;
+}
