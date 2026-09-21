627151147238810b4e1faf67b3a71bca6f506469
Wojciech Wojcik <wojcik.wojtek@yahoo.pl>
2026-09-21 19:07:58 +0200

    feat(gallery): add responsive gallery and image lightbox

--- src/Controller/GalleryController.php
+++ src/Controller/GalleryController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Enum\GalleryType;
+use App\Repository\PictureGalleryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,10 +12,79 @@ final class GalleryController extends AbstractController
 {
     #[Route('/gallery', name: 'gallery', methods: ['GET'])]
     #[Route('/pl/gallery', name: 'gallery_pl', methods: ['GET'])]
-    public function index(): Response
-    {
+    public function index(
+        PictureGalleryRepository $pictureGalleryRepository,
+    ): Response {
+
+        // Get list of pictures :)
+        $pictureList = $pictureGalleryRepository->getSortedGalleryPictures(GalleryType::BACKDROPS);
+
         return $this->render('gallery/index.html.twig', [
             'controller_name' => 'GalleryController',
+            'title' => 'Gallery',
+            'pictures' => $pictureList,
+        ]);
+    }
+
+    #[Route(
+        path: [
+            'en' => '/gallery/backdrop',
+            'pl' => '/pl/gallery/backdrop',
+        ],
+        name: 'app_backdrop',
+        methods: ['GET'],
+    )]
+    public function backdrop(
+        PictureGalleryRepository $pictureGalleryRepository,
+    ): Response {
+        $pictures = $pictureGalleryRepository->getSortedGalleryPictures(GalleryType::BACKDROPS);
+
+        return $this->render('gallery/index.html.twig', [
+            'controller_name' => 'GalleryController',
+            'title' => 'Backdrops',
+            'pictures' => $pictures,
+        ]);
+    }
+
+    #[Route(
+        path: [
+            'en' => '/gallery/animal',
+            'pl' => '/pl/gallery/animal',
+        ],
+        name: 'app_animal',
+        methods: ['GET'],
+    )]
+    public function animal(
+        PictureGalleryRepository $pictureGalleryRepository,
+    ): Response {
+        $pictures = $pictureGalleryRepository->getSortedGalleryPictures(GalleryType::ANIMALS);
+
+        return $this->render('gallery/index.html.twig', [
+            'controller_name' => 'GalleryController',
+            'title' => 'Animals',
+            'pictures' => $pictures,
+
+        ]);
+    }
+
+    #[Route(
+        path: [
+            'en' => '/gallery/event',
+            'pl' => '/pl/gallery/event',
+        ],
+        name: 'app_event',
+        methods: ['GET'],
+    )]
+    public function event(
+        PictureGalleryRepository $pictureGalleryRepository,
+    ): Response {
+        $pictures = $pictureGalleryRepository->getSortedGalleryPictures(GalleryType::DECORATIONS);
+
+        return $this->render('gallery/index.html.twig', [
+            'controller_name' => 'GalleryController',
+            'title' => 'Events',
+            'pictures' => $pictures,
+
         ]);
     }
 }
